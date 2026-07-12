9da478a191d413361d5e2cb8422c8315107c8884
legioner9 <legioner9@inbox.ru>
2026-07-12 21:10:38 +0700

    <>

--- .d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.tst.sh
+++ .d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.tst.sh
@@ -9,5 +9,10 @@
 # fns_bsh_001_dt5f ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001 \
 	# ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/.tst/dst 
 
+#.. l_02_fs2f ins.f {{pre_str_in_rcv.f}} rcv.f
 
+l_02_fs2f ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.dta.d/fns_bsh_001_dt5f.dta.d/.tml/fl_nm.arg.sh.tml {{fl_nm.arg}} ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001/fl_nm.arg.sh
 
+. ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001/fl_nm.arg.sh
+
+l_00_echo_warn "REMOVE COMMENTS{#..} in ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001/fl_nm.arg.sh "
