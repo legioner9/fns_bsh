450388192bc82c260ceb9138330d8c4b42f43998
legioner9 <legioner9@inbox.ru>
2026-07-12 20:52:04 +0700

    <>

--- .d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.tst.sh
+++ .d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.tst.sh
@@ -4,3 +4,10 @@
 . ~/fns_bsh/.d/.p/.p001.d/cp_this.sh \
 	~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d \
 	fns_bsh_001_dt5f.p001
+
+# in ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001/fl_nm.arg.sh ::
+# fns_bsh_001_dt5f ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/fns_bsh_001_dt5f.p001 \
+	# ~/fns_bsh/.d/.sh/.lib.sh/.fns_bsh_001.lib.tst.d/fns_bsh_001_dt5f.tst.d/.tst/dst 
+
+
+
