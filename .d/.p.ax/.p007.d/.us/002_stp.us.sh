d4cfb5ec8fb2457019c7dcbd6fb66c2eca4234e3
legioner9 <legioner9@inbox.ru>
2026-08-04 23:08:48 +0700

    <>

--- .d/.p.ax/.p007.d/.us/002_stp.us.sh
+++ .d/.p.ax/.p007.d/.us/002_stp.us.sh
@@ -3,8 +3,15 @@ l_00_echo_info "that :: 002_stp.us.sh"
 # echo "dr_pth_fn=$(eval "echo \$dr_pth_fn_${rnd}")"             # ~/fns_bsh/.d/.p.ax/.p007.d
 # echo "prnt1_dr_pth_fn=$(eval "echo \$prnt1_dr_pth_fn_${rnd}")" # ~/fns_bsh/.d/.p.ax
 # $(eval "echo \$prnt1_dr_pth_fn_${rnd}")/.p$(eval "echo \$arg_1_fn_${rnd}").d
-l_00_echo_info "gig in $(eval "echo \$prnt1_dr_pth_fn_${rnd}")/.p$(eval "echo \$arg_1_fn_${rnd}").d/g.pr"
-fns_bsh_001_als_gig_fl_sh 
 
+l_00_echo_info "gig $(eval "echo \$prnt1_dr_pth_fn_${rnd}")/.p$(eval "echo \$arg_1_fn_${rnd}").d/g.pr"
 
+fns_bsh_001_als_gig_fl_sh $(eval "echo \$prnt1_dr_pth_fn_${rnd}")/.p$(eval "echo \$arg_1_fn_${rnd}").d \
+	g.pr \
+	~/fns_bsh/.d/.p.ax/.cmn/.cmn.tml.d/001.cmn.tml.d \
+	~/fns_bsh/.d/.p.ax/.dom/.dom.tml.d/002.dom.tml.d
 
+fns_bsh_001_als_gig_fl_sh $(eval "echo \$prnt1_dr_pth_fn_${rnd}")/.p$(eval "echo \$arg_1_fn_${rnd}").d/.tst \
+	g.tst \
+	~/fns_bsh/.d/.p.ax/.cmn/.cmn.tml.d/001.cmn.tml.d \
+	~/fns_bsh/.d/.p.ax/.dom/.dom.tml.d/003.dom.tml.d
