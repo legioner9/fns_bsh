ce44ce614cf04d8846e21e592e815f1b616fa207
legioner9 <legioner9@inbox.ru>
2026-08-08 12:15:56 +0700

    <>

--- .d/.p.ax/.p009.d/.us/002_f2f.us.sh
+++ .d/.p.ax/.p009.d/.us/002_f2f.us.sh
@@ -1,1 +1,9 @@
 l_00_echo_info "that :: 002_f2f.us.sh"
+# res fl -> $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")
+# l_02_s2f :: $1 reciver_string: $2 inserter_string[@ - empty string] in reciver_result_file: $3
+
+l_02_s2f {{fl_pth}} $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")  $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")
+
+# normalize pth
+
+lfoe_path_to_var $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")
