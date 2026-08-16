ebb4f678708a751ee47d15a54f91726874544c63
legioner9 <legioner9@inbox.ru>
2026-08-16 19:16:04 +0700

    <>

--- .d/.p.ax/.p012.d/.us/002.us.sh
+++ .d/.p.ax/.p012.d/.us/002.us.sh
@@ -1,2 +1,7 @@
 # from:: ~/fns_bsh/.d/.p.ax/.p007.d/.dta/.pXXX.dtml/.us/001_gig_tml.us.sh
 l_00_echo_info "that :: 002.us.sh"
+
+fns_bsh_001_als_gig_fl_sh $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}") \
+	start_make.sh \
+	~/fns_bsh/.d/.p.ax/.cmn/.cmn.tml.d/001.cmn.tml.d \
+	~/fns_bsh/.d/.p.ax/.p012.d/.dta/g_arg.dom.tml.d
