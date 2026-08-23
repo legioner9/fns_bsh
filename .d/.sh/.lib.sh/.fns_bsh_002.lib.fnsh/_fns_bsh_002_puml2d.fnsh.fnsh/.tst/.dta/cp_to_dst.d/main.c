f1b44fbc23fd459b78082158ddff6c7f7320d97d
legioner9 <legioner9@inbox.ru>
2026-08-23 14:51:23 +0700

    <>

---
+++ .d/.sh/.lib.sh/.fns_bsh_002.lib.fnsh/_fns_bsh_002_puml2d.fnsh.fnsh/.tst/.dta/cp_to_dst.d/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// That flow: 2 file compile without .h files
+// @@@anc https://labex.io/ru/tutorials/c-manage-projects-with-make-in-c-438333
+// {{hint}}
+
+// page book:: XXX
+
+void print_utils();
+
+int main(){
+    printf("%s","Make Word that)))\n");
+    print_utils();
+    return 0;
+}
