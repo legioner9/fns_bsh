343da2be6a3e8e02fa8e5750945f6c9df9c07e1a
legioner9 <legioner9@inbox.ru>
2026-08-24 10:09:00 +0700

    <>

---
+++ .d/.sh/.lib.sh/.fns_bsh_002.lib.fnsh/fns_bsh_002_puml2d.fns/.us/001.us.sh
@@ -0,0 +1,24 @@
+# from:: ~/fns_bsh/.d/.p.ax/.p007.d/.dta/.pXXX.dtml/.us/001.us.sh
+l_00_echo_info "that :: 001.us.sh"
+# ~001_001_us_sh~
+	# from:: ~/fns_bsh/.d/.p.ax/.cmn/.dom.tml.d/0012.dom.tml.d/001_001_us_sh.tml
+	# first:: for .p015.d gig 001.us.sh
+
+# check exist $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")
+echo -e " arg_flow ::
+if src_dir :: $(eval "echo \$dr_pth_fn_${rnd}")/.dta/cp_to_dst.d
+\$1 root_dr \$2 ::
+dr - result dr
+fl - result fl
+"
+eval "flow_1_${rnd}=dr"
+# $(eval "echo \$flow_1_${rnd}")
+
+if [[ $(eval "echo \$flow_1_${rnd}") == "dr" ]]; then
+
+	:
+
+fi
+
+	#-- {{002_001_us_sh}}
+		
