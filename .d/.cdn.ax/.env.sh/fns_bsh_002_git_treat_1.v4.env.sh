e67b5247dcb2e7ecad3465b7e7b003b80bafab46
legioner9 <legioner9@inbox.ru>
2026-08-27 19:02:48 +0700

    <>

--- .d/.cdn.ax/.env.sh/fns_bsh_002_git_treat_1.v4.env.sh
+++ .d/.cdn.ax/.env.sh/fns_bsh_002_git_treat_1.v4.env.sh
@@ -2,39 +2,11 @@
 
 l_00_echo_ques "PART_PROGRAM_IS_START :: get and check env for fns_bsh_002_git_treat_1"
 
-#! check and cp to ~fns_bsh/.d/.sh/.lib.sh/.fns_bsh_002.lib.fnsh/fns_bsh_002_git_treat_1.fnsh/fns_bsh_002_git_treat_1.sh
-# local item=
-# local item1=
-# local item2=
-# local item3=
-# local item4=
-# local item5=
-
-# local fns_bsh_002_git_treat_1_arr_remote_addr=
-# local fns_bsh_002_git_treat_1_arr_remote_alias=
-# local fns_bsh_002_git_treat_1_arr_rpo_pth=
-# local fns_bsh_002_git_treat_1_arr_remote_local=
-# local fns_bsh_002_git_treat_1_arr_remote_local_alias=
-# local fns_bsh_002_git_treat_1_main_repo=
-# local fns_bsh_002_git_treat_1_ping=
-
-# fns_bsh_002_git_treat_1_arr_remote_addr=(
-# 	git@gitflic.ru:legioner9
-# 	git@github.com:legioner9
-# 	ssh://forgejo@altlinux.space/legioner9)
-
-# fns_bsh_002_git_treat_1_arr_remote_alias=(gf gh alt)
 #! exa repo 
 fns_bsh_002_git_treat_1_arr_rpo_pth=(
-	rpo/exa
+	rpo/1/exa
 	# TTT/tml_a
 )
-
-# fns_bsh_002_git_treat_1_arr_remote_addr_and_alias=(
-# 	"gf git@gitflic.ru:legioner9"
-# 	"gh git@github.com:legioner9"
-# 	"alt ssh://forgejo@altlinux.space/legioner9"
-# )
 
 declare -A fns_bsh_002_git_treat_1_arr_remote
 
