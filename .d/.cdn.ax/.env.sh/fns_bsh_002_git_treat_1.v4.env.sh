98370a418c82e27b8816da108bb3386a55259ddb
legioner9 <legioner9@inbox.ru>
2026-08-27 18:44:53 +0700

    <>

--- .d/.cdn.ax/.env.sh/fns_bsh_002_git_treat_1.v4.env.sh
+++ .d/.cdn.ax/.env.sh/fns_bsh_002_git_treat_1.v4.env.sh
@@ -24,7 +24,7 @@ l_00_echo_ques "PART_PROGRAM_IS_START :: get and check env for fns_bsh_002_git_t
 # 	ssh://forgejo@altlinux.space/legioner9)
 
 # fns_bsh_002_git_treat_1_arr_remote_alias=(gf gh alt)
-
+#! exa repo 
 fns_bsh_002_git_treat_1_arr_rpo_pth=(
 	rpo/exa
 	# TTT/tml_a
