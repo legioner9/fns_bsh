919b37a1f6d89553cb83e89e9289b8671809a392
legioner9 <legioner9@inbox.ru>
2026-08-29 06:23:24 +0700

    <>

---
+++ .d/.sh/.lib.sh/.fns_bsh_002.lib.fnsh/fns_bsh_002_d2md_1.fnsh/.us/001.us.sh
@@ -0,0 +1,44 @@
+# from:: ~/fns_bsh/.d/.p.ax/.p007.d/.dta/.pXXX.dtml/.us/001.us.sh
+l_00_echo_info "that :: 001.us.sh"
+l_00_echo_ques "... DFN:: fn()...| BODY::..."
+# ~001_001_us_sh~
+	# from:: ~/fns_bsh/.d/.p.ax/.cmn/.dom.tml.d/0012.dom.tml.d/001_001_us_sh.tml
+	# first:: for .p015.d gig 001.us.sh
+
+# check exist $(eval "echo \$arg_1_fn_${rnd}")/$(eval "echo \$arg_2_fn_${rnd}")
+echo -e " arg_flow ::
+if src_dir :: $(eval "echo \$dr_pth_fn_${rnd}")/.dta/cp_to_dst.d
+\$1 root_dr \$2 ::
+dr - result dr
+fl - result fl
+"
+# arg1 ::
+# $(eval "echo \$arg_1_fn_${rnd}")
+# arg2 ::
+# $(eval "echo \$arg_2_fn_${rnd}")
+
+# fl_pth_fn ::
+# $(eval "echo \$fl_pth_fn_${rnd}")
+# dr_pth_fn ::
+# $(eval "echo \$dr_pth_fn_${rnd}")
+# fl_nm_fn ::
+# $(eval "echo \$fl_nm_fn_${rnd}")
+# prnt1_dr_pth_fn ::
+# $(eval "echo \$prnt1_dr_pth_fn_${rnd}")
+# prnt2_dr_pth_fn ::
+# $(eval "echo \$prnt2_dr_pth_fn_${rnd}")
+
+eval "flow_1_${rnd}=dr"
+# $(eval "echo \$flow_1_${rnd}")
+
+# up to_mem .env.sh dr
+l_01_c_up $(eval "echo \$dr_pth_fn_${rnd}")/.env.sh
+
+if [[ $(eval "echo \$flow_1_${rnd}") == "dr" ]]; then
+
+	:
+
+fi
+
+	#-- {{002_001_us_sh}}
+		
